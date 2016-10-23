e89caa76987130b464ec03f5cc1e768a6d26a9a0
Dennis-Numjew <dennis_numjew@hotmail.com>
2016-10-23 21:53:44 +0100

    Initial test to start the assignment

---
+++ HMRC/src/test/java/Checkout.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Checkout {
+
+    @Test
+    public void processCheckout(){
+
+        System.out.print("Test failed.");
+
+        //Assert.fail("Test failed.");
+    }
+}
