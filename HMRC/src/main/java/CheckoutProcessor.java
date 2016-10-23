9426b9a4d2cca500ff6f90f872105efcd4e177eb
Dennis-Numjew <dennis_numjew@hotmail.com>
2016-10-23 22:01:12 +0100

    Add calculating total cost of shopping card. Using Java Stream to do the calculation in functional way

--- HMRC/src/main/java/CheckoutProcessor.java
+++ HMRC/src/main/java/CheckoutProcessor.java
@@ -1,9 +1,22 @@
 package main.java;
 
+import java.util.Arrays;
+
 public class CheckoutProcessor {
 
     public static double checkout(String[] items){
 
-        return 0.85;
+        double total = Arrays.stream(items).map(item -> {
+            switch (item){
+                case "Apple":
+                    return new Double(0.60);
+                case "Orange":
+                    return new Double(0.25);
+                default:
+                    return new Double(0.0);
+            }
+        }).mapToDouble(Double::doubleValue).sum();
+
+        return total;
     }
 }
